1b55a51aa0bc2190e3e2d72ba624b12ed5626c8f
kissesy <b7712983@naver.com>
2020-08-24 16:37:01 +0900

    application

---
+++ application/HelloXamarin/HelloXamarin.Android/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.Media;
+using Android.Support.V4.Content;
+using Android.Support.V4.App;
+
+namespace HelloXamarin.Droid
+{
+    [Activity(Label = "HelloXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            await CrossMedia.Current.Initialize();
+            
+            int requestPermissions = 0;
+            string cameraPermission = Android.Manifest.Permission.Camera;
+
+            if (!(ContextCompat.CheckSelfPermission(this, cameraPermission) == (int)Permission.Granted))
+            {
+                ActivityCompat.RequestPermissions(this, new String[] { cameraPermission, }, requestPermissions);
+            }
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            //Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ application/HelloXamarin/HelloXamarin/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloXamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //this.BindingContext = this;
+            this.BindingContext = new MainViewModel();
+        }
+        /*
+        private bool _OnRecipe;
+        public bool OnRecipe
+        {
+            get { return _OnRecipe; }
+            set { _OnRecipe = value; RaisePropertyChanged("OnRecipe"); }
+        }
+
+        private string _RecommendFood;
+        public string RecommendFood
+        {
+            get { return _RecommendFood; }
+            set { _RecommendFood = value; RaisePropertyChanged("RecommendFood"); }
+        }
+
+
+        private async void takePhoto_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            MediaFile file;
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
+            try
+            {
+                //Exception occurs in this code.
+                file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    //Specify Store to Album OR Directory, not both
+                    Directory = "App_Images",
+                    Name = "Test.jpg"
+
+                });
+            }
+            catch (Exception exception)
+            {
+                await DisplayAlert("Error", exception.Message, "OK");
+                //I've got a break point here which is being hit, but the exception is (null)
+                throw;
+            }
+
+            if (file == null)
+                return;
+
+            RecommendFood = "asda";
+        }
+
+        #region INotify
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+        #endregion
+        */
+    }
+}
+    
